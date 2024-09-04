6386ccafc16e46e82b9cc19244a6e89cf85b2dab
Muhammad Zahooruddin Nizamani <84314402+Zahooruddin-dev@users.noreply.github.com>
2024-09-04 21:56:46 +0500

    Job done
    nothing fancy just a simple Wikipedia search Site using their API

---
+++ README.md
@@ -0,0 +1,14 @@
+
+
+--------- It's a simple Wikipedia search site you can't open any wiki within the site you will be redirected
+
+
+#### API DOCS
+
+- [wiki docs](https://www.mediawiki.org/wiki/API:Main_page) 
+
+
+
+
+
+
---
+++ wiki-urls.js
@@ -0,0 +1,11 @@
+const url =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=searchValue';
+
+// list=search - perform a full text search
+// srsearch="inputValue" - search for page titles or content matching  this value.
+// srlimit=20 How many total pages to return.
+// format=json json response
+// "origin=*" fix cors errors
+//  This is the file used for the API calling
+
+const page_url = 'href=http://en.wikipedia.org/?curid=${pageid}';
